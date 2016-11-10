e481028bf6c85430a0203c1b82e6e696dee0be6d
Ashish_Nagdev <ashunagdev@gmail.com>
2016-11-10 11:51:17 -0500

    Main class for spark credit risk

---
+++ src/main/scala/com/sherpa/ml/main/Main.scala
@@ -0,0 +1,32 @@
+package com.sherpa.ml.main
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+import com.sherpa.ml.creditrisk.CreditParser._
+import com.sherpa.ml.creditrisk._
+
+/**
+  * Created by Ashish Nagdev on 11/10/16.
+  */
+object Main {
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("SparkDFebay").setMaster("local")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    import sqlContext._
+    import sqlContext.implicits._
+
+    val creditDF = parseRDD(sc.textFile("credit.csv")).map(parseCredit).toDF().cache()
+    creditDF.registerTempTable("credit")
+    creditDF.printSchema
+
+    creditDF.show
+    sqlContext.sql("SELECT creditability, avg(balance) as avgbalance, avg(amount) as avgamt, avg(duration) as avgdur  FROM credit GROUP BY creditability ").show
+
+    creditDF.describe("balance").show
+    creditDF.groupBy("creditability").avg("balance").show
+    new CreditRisk(creditDF)
+  }
+
+}
