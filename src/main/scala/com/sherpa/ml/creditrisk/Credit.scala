14ecb9147a4c42e7b29fe1a2baf930d58b090fe8
Ashish_Nagdev <ashunagdev@gmail.com>
2016-11-15 07:52:35 -0500

    Sample data to generate Credit class

--- src/main/scala/com/sherpa/ml/creditrisk/Credit.scala
+++ src/main/scala/com/sherpa/ml/creditrisk/Credit.scala
@@ -3,6 +3,11 @@ package com.sherpa.ml.creditrisk
 /**
   * Created by Ashish Nagdev on 11/10/16.
   */
+
+//Creating Credit class for data:
+//
+//1,1,18,4,2,1049,1,2,4,2,1,4,2,21,3,1,1,3,1,1,1
+
 case class Credit(
                    creditability: Double,
                    balance: Double, duration: Double, history: Double, purpose: Double, amount: Double,
